d25c5e2c14b1fc3ba5923a7fe73a693a82f48497
tyurome <leonleon1215@gmail.com>
2024-04-24 06:38:56 +0900

    change:簡単ログイン機能

--- app/main.py
+++ app/main.py
@@ -1,12 +1,29 @@
-#Web開発フレームワーク及びテンプレートエンジン
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, User
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Flask!'
+def index():
+    return redirect(url_for('login'))
 
-#Web鯖起動
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            return 'Login Success!'
+        else:
+            return 'Invalid Credentials'
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
