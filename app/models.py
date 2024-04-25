35d3c1fb54cb6a86e43e3bb5aa16fd5299a0e0df
tyurome <leonleon1215@gmail.com>
2024-04-25 12:20:31 +0900

    add:db model for usermixin

--- app/models.py
+++ app/models.py
@@ -1,11 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(UserMixin, db.Model): #UserMixin...flask_loginの機能を使うためのクラス
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
