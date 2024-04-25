294daa976cb026a599c9e4e745a15f0e453551d1
tyurome <leonleon1215@gmail.com>
2024-04-26 07:59:39 +0900

    add:mvc

--- app/main.py
+++ app/main.py
@@ -1,68 +1,39 @@
-from flask import Flask, render_template, request, redirect, url_for
+#Webフレームワーク
+from flask import Flask, url_for
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User
+
+#入出力管理用
 import os
 
 app = Flask(__name__)
 
 #db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.urandom(24) #secret key for session
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.database.db' #sqliteのDBファイルを指定
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #DB変更時の追跡機能を無効可（推奨はFalse）
+app.config['SECRET_KEY'] = os.urandom(24) #session情報の暗号化キーを生成
 db.init_app(app) #db modelを初期化
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login' #route to login
+login_manager.login_view = 'main.login' #セッション情報が無い場合にリダイレクトするページの指定
 
+#ログインマネージャ、セッション情報の取得
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/')
-def index():
-    return redirect(url_for('/login'))
+from controller import main
+app.register_blueprint(main)
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+@app.route('/test')
+def test():
+    return url_for('main.login')
 
-        user = User(username=username, password=generate_password_hash(password, method='pbkdf2:sha256'))
-        db.session.add(user)
-        db.session.commit()
-        return redirect('/login')
-    else:
-        return render_template('register.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        user = User.query.filter_by(username=username).first() #ユーザ名がユニークなのでfirst（一番上）指定でいいかな
-        if check_password_hash(user.password, password):
-            login_user(user)
-            return redirect('/home')
-    else:
-        return render_template('login.html')
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-@app.route('/home')
-@login_required
-def protected():
-    return 'Logged in as: ' + current_user.username
-
+#アプリケーションの起動
 if __name__ == '__main__':
+    #DBの初期化
     with app.app_context():
         db.create_all()
+    #指定のIPアドレスとポート番号でアプリケーションを起動
     app.run(host="0.0.0.0", port=5000)
